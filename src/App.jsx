7d6bc69fc587c00166b9d4358189cb64d4175081
sadra <sadraansari86@gmail.com>
2026-09-05 15:51:09 +0330

    need to be continued

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Body from "./components/body";
+import "./App.css";
+
+export default function App() {
+    return <Body />;
+}
---
+++ src/components/body.jsx
@@ -0,0 +1,30 @@
+export default function Body() {
+    return (
+        <div className="body">
+            <form>
+                <div className="input_div">
+                    <input type="text" />
+                </div>
+                <div className="buttons">
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                    <button type="button"></button>
+                </div>
+            </form>
+        </div>
+    );
+}
